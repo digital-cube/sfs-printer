4feecebca6191a2a6c2a55ca03b9a8365ef80625
Igor Jeremic <igor@digitalcube.rs>
2021-11-11 00:28:36 +0100

    i

--- sfsqr.py
+++ sfsqr.py
@@ -20,8 +20,8 @@ def main():
     }
 
     #gen(r)
-    a  = generate(r)
-    print("A",a)
+    a  = generate2(r)
+    #print("A",a)
     
 def gen(r):
 
@@ -42,10 +42,10 @@ def gen(r):
         if width < img.size[0]:
             break
     
-    print(f'addig {text} using {fsize}')
+    #print(f'addig {text} using {fsize}')
     img_draw.text((img.size[0] / 2 - width / 2, 0), text, fill='black', font=font)
 
-    print('x',img.save('res.png'))
+    #print('x',img.save('res.png'))
     
     
 def generate(r):
@@ -58,7 +58,7 @@ def generate(r):
 
     noi = PIL.Image.open('noi.png')#.convert('RGB')
 
-    print("SHOW")
+    #print("SHOW")
 
     
     img_draw = PIL.ImageDraw.Draw(img2)
@@ -75,13 +75,13 @@ def generate(r):
 #    img_draw.text((img.size[0] / 2 - tw / 2, 0), r['data']['title'], fill='black', font=font)   
 #    offset += th + 20
 
-    print(noi.size)
+    #print(noi.size)
 
-    x=0.1
+    x=0.075
         
     size=(int(noi.size[0]*x), int(noi.size[1]*x))    
     
-    print(size)
+    #print(size)
 
     noi = noi.resize( size, PIL.Image.BICUBIC)
     
@@ -104,7 +104,7 @@ def generate(r):
         if width < img2.size[0]:
             break
     
-    print(f'addig {text} using {fsize}')
+    #print(f'addig {text} using {fsize}')
     img_draw.text((img.size[0] / 2 - width / 2, offset), text, fill='black', font=font)
     offset += height + 10
     
@@ -121,7 +121,7 @@ def generate(r):
             if cwidth < img2.size[0]:
                 break
     
-        print(f'addig {text} using {fsize}')
+        #print(f'addig {text} using {fsize}')
         img_draw.text((img.size[0] / 2 - cwidth / 2, offset), text, fill='black', font=font)
     
         offset += cheight + 10
@@ -131,6 +131,62 @@ def generate(r):
     fname = f'/tmp/label-{qrcodeid}.png'
     img2.save(fname)
     return fname
+    
+    
+def generate2(r):
+    
+    qrcodeid = r['data']['qrcodeid']
+
+    width = 720
+    height = 480
+    
+    bckg = PIL.Image.new("1", (width, height), (255))
+    
+    qrimg = qrcode.make(qrcodeid)
+    qrimg = qrimg.resize((220,220),PIL.Image.ANTIALIAS)
+
+    with open('sfs2021.png') as f:
+        sfsimg = PIL.Image.open("sfs2021.png")
+
+        sx = 0.85
+        #print(sfsimg.size)
+        s = sfsimg.size
+        s = (int(s[0]*sx), int(s[1]*sx))
+        sfsimg = sfsimg.resize(s)
+        #print(s)
+
+
+    img_draw = PIL.ImageDraw.Draw(bckg)
+
+    fsize=60
+    font = PIL.ImageFont.truetype("font-bold.ttf", fsize)    
+    font2 = PIL.ImageFont.truetype("font.ttf", fsize)    
+
+    dname = r['data']['first_name']+' '+r['data']['last_name']
+
+    if len(dname)>18:
+        img_draw.text((32,32), r['data']['first_name'], fill='black', font=font)  
+        img_draw.text((32,108), r['data']['last_name'], fill='black', font=font)  
+        img_draw.text((32,184), r['data']['company'], fill='black', font=font2)  
+    else:
+        img_draw.text((32,32), r['data']['first_name']+' '+r['data']['last_name'], fill='black', font=font)  
+        img_draw.text((32,108), r['data']['company'], fill='black', font=font2)  
+
+    
+    bckg.paste(qrimg,(bckg.size[0]-10-qrimg.size[0],
+                      bckg.size[1]-10-qrimg.size[1]))
+    
+    bckg.paste(sfsimg,(32, 350))
+    
+    
+
+
+    fname = f'/tmp/label-{qrcodeid}.png'
+    bckg.save(fname)
+
+    
+if __name__=='__main__':
+    main()
     
 if __name__=='__main__':
     main()
